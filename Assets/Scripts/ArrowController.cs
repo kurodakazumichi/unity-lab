224c59bbec8c38462b34c4a21feb03e05113986f
黒田一道 <>
2018-10-17 00:19:36 +0900

    Add ArrowController Scripts

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, -0.1f, 0);
+
+        if (transform.position.y < -5.0f) {
+            Destroy(this.gameObject);
+        }
+	}
+}
