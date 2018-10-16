0ca333f3b40e4587ca7ba4231d9ea1c8564e73af
黒田一道 <>
2018-10-17 00:29:49 +0900

    Add  プレイヤーとの衝突判定

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour {
 
+    GameObject player;
+
 	// Use this for initialization
 	void Start () {
+
+        this.player = GameObject.Find("player");
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
         transform.Translate(0, -0.1f, 0);
 
         if (transform.position.y < -5.0f) {
+            Destroy(this.gameObject);
+        }
+
+        Vector2 dir = this.transform.position - player.transform.position;
+        if(dir.magnitude < 1.5f) {
             Destroy(this.gameObject);
         }
 	}
