a56d4e2ba2ce827c58f40cab9f69ea93cd8af0be
黒田一道 <>
2018-10-17 22:27:33 +0900

    Modified UIButtonに対応

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,16 +8,12 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
-            transform.Translate(-3, 0, 0);
-        }
-		
-        if(Input.GetKeyDown(KeyCode.RightArrow)) {
-            transform.Translate(3, 0, 0);
-        }
-	}
+
+    public void LButtonDown() {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RButtonDown() {
+        transform.Translate(3, 0, 0);
+    }
 }
