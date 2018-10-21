1c046341cdf22cffa3d7de5f2b3521b5e136c519
黒田一道 <>
2018-10-21 20:57:03 +0900

    Add Player2

---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour {
+
+    Rigidbody2D rigid2D;
+    float jumpForce = 780.0f;
+    float walkForce = 30.0f;
+    float maxWaldSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // 左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // プレイヤの速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        if(speedx < this.maxWaldSpeed) {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        // 動く方向に応じて反転
+        if(key != 0) {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+	}
+}
